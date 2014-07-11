70535f3e3259503beff5d5c2aae748a5651b10a7
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-10 18:18:43 -0700

    feat(hint-log): add methods for printing hint warnings to the console

--- hint-log.js
+++ hint-log.js
@@ -1,7 +1,111 @@
 (function (hintLog) {
-  hintLog.logMessages = function(messages, module) {
-    return true;
+
+  hintLog.throwError = false;
+  hintLog.debugBreak = false;
+  hintLog.propOnly = false;
+  hintLog.includeLine = true;
+  hintLog.setLogDefault = function(defaultToSet, status) {
+    if(defaultToSet === 'throwError') {
+      status ? hintLog.throwError = true : hintLog.throwError = false;
+    }
+    else if(defaultToSet === 'debuggerBreakpoint') {
+      status ? hintLog.debugBreak = true : hintLog.debugBreak = false;
+    }
+    else if(defaultToSet === 'propertyOnly') {
+      status ? hintLog.propOnly = true : hintLog.propOnly = false;
+    }
+    else if(defaultToSet === 'includeLine') {
+      status ? hintLog.includeLine = true : hintLog.includeLine = false;
+    }
+    else {
+      throw new Error('Tried to set unknown log default: ' + defaultToSet);
+    }
+  };
+
+  //Record past messages so that the same line number will not
+  //be repeatedly reported
+  hintLog.pastMessages = {};
+  hintLog.currentMessages = [];
+  hintLog.lines = [];
+  hintLog.domElements = {};
+  hintLog.moduleName;
+  hintLog.moduleDescription;
+
+
+  //Log messages periodically
+  hintLog.printAvailableMessages = function(printFrequency) {
+    setTimeout(function() {
+      if(hintLog.currentMessages.length > 0) {
+        //Provide formatted messages if browsers allow
+        if(console.groupCollapsed && console.warn) {
+          hintLog.logFormattedMessages();
+        }
+        //Default to console.log which is available in all browsers
+        else {
+          hintLog.logMessages();
+        }
+      }
+    }, printFrequency);
+  };
+
+  hintLog.printAvailableMessages(500);
+
+  hintLog.logFormattedMessages = function() {
+    console.groupCollapsed('Angular Hint: ' + hintLog.moduleName + ' ' + hintLog.moduleDescription);
+    for(var i = 0; i < hintLog.currentMessages.length; i++) {
+      console.warn(hintLog.currentMessages[i] + ' ' + hintLog.lines[i]);
+      if(hintLog.moduleName === 'Directives') {
+        console.log(hintLog.domElements[hintLog.lines[i]]);
+      }
+    }
+    console.groupEnd();
+  };
+
+  hintLog.logMessages = function() {
+    console.log('Angular Hint: ' + hintLog.moduleName + ' ' + hintLog.moduleDescription);
+    for(var i = 0; i < hintLog.currentMessages.length; i++) {
+      console.log(hintLog.currentMessages[i] + ' ' + hintLog.lines[i]);
+      if(hintLog.moduleName === 'Directives') {
+        console.log(hintLog.domElements[hintLog.lines[i]]);
+      }
+    }
   }
+
+  hintLog.foundError = function(error) {
+    if(hintLog.debugBreak) {
+      debugger;
+    }
+    else if(hintLog.loudError) {
+      throw new Error(error + ' ' + hintLog.findLineNumber());
+    }
+    else {
+      if(moduleName === 'Directives') {
+        hintLog.createErrorMessage(error, hintLog.findLineNumber(), domElement);
+      }
+      else {
+        hintLog.createErrorMessage(error, hintLog.findLineNumber());
+      }
+    }
+  };
+
+  hintLog.findLineNumber = function() {
+    var e = new Error();
+    //Find the line in the user's program rather than in this service
+    var lineNum = e.stack.split('\n')[4];
+    lineNum = lineNum.split('<anonymous> ')[1];
+    return lineNum;
+  };
+
+  hintLog.createErrorMessage = function(error, lineNumber, domElement) {
+    if(!hintLog.pastMessages[lineNumber]) {
+      hintLog.pastMessages[lineNumber] = lineNumber;
+      hintLog.currentMessages.push(error);
+      domInterceptor.lines.push(lineNumber);
+      if(domElement) {
+        domElements[lineNumber] = domElement;
+      }
+    }
+  };
 
 }((typeof module !== 'undefined' && module && module.exports) ?
       (module.exports = window.hintLog = {}) : (window.hintLog = {}) ));
--- hint-log_test.js
+++ hint-log_test.js
@@ -1,8 +1,83 @@
 describe('hintLog', function() {
 
+  describe('setLogDefaults', function() {
+    it('should set default properties to true or false', function() {
+      expect(hintLog.throwError).toBe(false);
+      hintLog.setLogDefault('throwError', true);
+      expect(hintLog.throwError).toBe(true);
+
+      expect(hintLog.debugBreak).toBe(false);
+      hintLog.setLogDefault('debuggerBreakpoint', true);
+      expect(hintLog.debugBreak).toBe(true);
+
+      expect(hintLog.propOnly).toBe(false);
+      hintLog.setLogDefault('propertyOnly', true);
+      expect(hintLog.propOnly).toBe(true);
+
+      expect(hintLog.includeLine).toBe(true);
+      hintLog.setLogDefault('includeLine', false);
+      expect(hintLog.includeLine).toBe(false);
+    });
+  });
+
+  describe('printAvailableMessages', function()  {
+    it('should print messages at the pace of the frequency parameter', function() {
+      spyOn(hintLog, 'logFormattedMessages');
+      hintLog.currentMessages = ['testMessage'];
+      runs(function() {
+        hintLog.printAvailableMessages(500);
+      });
+
+      waits(501);
+
+      runs(function() {
+        expect(hintLog.logFormattedMessages).toHaveBeenCalled();
+      });
+    });
+  });
+
+  describe('logFormattedMessages', function() {
+    it('should print messages using groupCollapsed and warn', function() {
+      spyOn(console, 'groupCollapsed');
+      spyOn(console, 'warn');
+
+      hintLog.moduleName = 'Test';
+      hintLog.moduleDescription = 'A module to test Angular hint logging'
+      hintLog.currentMessages = ['testMessage'];
+      hintLog.logFormattedMessages();
+      expect(console.groupCollapsed).toHaveBeenCalled();
+      expect(console.warn).toHaveBeenCalled();
+    });
+
+
+    it('should console log an element if called by the Directives module', function() {
+      spyOn(console, 'groupCollapsed');
+      spyOn(console, 'warn');
+      spyOn(console, 'log');
+
+      hintLog.moduleName = 'Directives';
+      hintLog.moduleDescription = 'A module to hint about Angular Directives'
+      hintLog.currentMessages = ['Did you mean ng-click?'];
+      hintLog.logFormattedMessages();
+      expect(console.groupCollapsed).toHaveBeenCalled();
+      expect(console.warn).toHaveBeenCalled();
+      expect(console.log).toHaveBeenCalled();
+    });
+  });
+
   describe('logMessages', function() {
-    it('should return true', function() {
-      expect(hintLog.logMessages()).toBe(true);
+    it('should print messages without any formatting using console.log', function() {
+      spyOn(console, 'groupCollapsed');
+      spyOn(console, 'warn');
+      spyOn(console, 'log');
+
+      hintLog.moduleName = 'Test';
+      hintLog.moduleDescription = 'A module to hint about Angular Directives'
+      hintLog.currentMessages = ['Did you mean ng-click?'];
+      hintLog.logMessages();
+      expect(console.groupCollapsed).not.toHaveBeenCalled();
+      expect(console.warn).not.toHaveBeenCalled();
+      expect(console.log).toHaveBeenCalled();
     });
   });
 });
