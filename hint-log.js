9d0881b0b27319aaefd14d77492a9c18870637c7
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-28 16:21:55 -0700

    feat(hintLog): change hintLog message queue from array to object
    Previously the message queue was a simple array of all messages. Now the object queue allows
    quick checks of messages to ensure that duplicate messages are not reported. Additionally, messages
    can be grouped by object keys according to their module of origin.

--- hint-log.js
+++ hint-log.js
@@ -1,12 +1,25 @@
-var queuedMessages = [];
+var queuedMessages = {};
 function logMessage(message) {
-  queuedMessages.push(message);
+  var nameAndValue = message.split(/##/);
+  if(nameAndValue[0] !== '') {
+    if(queuedMessages['No Name']) {
+      queuedMessages['No Name'][message] = [message];
+    }  else {
+      queuedMessages['No Name'] = {};
+      queuedMessages['No Name'][message] = [message];
+    }
+  } else if(queuedMessages[nameAndValue[1]]) {
+    queuedMessages[nameAndValue[1]][nameAndValue[2]] = nameAndValue[2];
+  } else {
+    queuedMessages[nameAndValue[1]] = {};
+    queuedMessages[nameAndValue[1]][nameAndValue[2]] = [nameAndValue[2]];
+  }
   module.exports.onMessage(message);
 };
 
 function flush() {
   var flushMessages = queuedMessages;
-  queuedMessages = [];
+  queuedMessages = {};
   return flushMessages;
 };
 
--- hint-log_test.js
+++ hint-log_test.js
@@ -8,8 +8,41 @@ describe('hintLog', function() {
 
   describe('logMessage', function() {
     it('should add a new message to the message queue', function() {
+      hintLog.logMessage('##Hint Directives## An error');
+      expect(hintLog.flush()['Hint Directives'][' An error']).toEqual([' An error']);
+    });
+
+
+    it('should identify the name of a module given between ##identifiers##', function() {
+      hintLog.logMessage('##Hint Dom## An error');
+      expect(hintLog.flush()['Hint Dom'][' An error']).toEqual([' An error']);
+    });
+
+
+    it('should queue modules without a given name under No Name', function() {
       hintLog.logMessage('An error');
-      expect(hintLog.flush()).toEqual(['An error']);
+      expect(hintLog.flush()['No Name']['An error']).toEqual(['An error']);
+    });
+
+
+    it('should prevent the logging of duplicate messages', function() {
+      //Same error, only logged once
+      hintLog.logMessage('##Hint Dom## An error');
+      hintLog.logMessage('##Hint Dom## An error');
+      expect(Object.keys(hintLog.flush()['Hint Dom']).length).toBe(1);
+
+      //Different errors, both logged
+      hintLog.logMessage('##Hint Dom## An error');
+      hintLog.logMessage('##Hint Dom## A second error');
+      expect(Object.keys(hintLog.flush()['Hint Dom']).length).toBe(2);
+
+      hintLog.logMessage('An error');
+      hintLog.logMessage('An error');
+      expect(Object.keys(hintLog.flush()['No Name']).length).toBe(1);
+
+      hintLog.logMessage('An error');
+      hintLog.logMessage('A second error');
+      expect(Object.keys(hintLog.flush()['No Name']).length).toBe(2);
     });
   });
 
@@ -17,14 +50,18 @@ describe('hintLog', function() {
     it('should return the currently queued messages', function() {
       hintLog.logMessage('An error');
       hintLog.logMessage('Another error');
-      expect(hintLog.flush()).toEqual(['An error', 'Another error']);
+      var log = hintLog.flush();
+      expect(log['No Name']['An error']).toEqual(['An error']);
+      expect(log['No Name']['Another error']).toEqual(['Another error']);
     });
 
 
     it('should empty the queued messages', function() {
       hintLog.logMessage('An error');
       hintLog.logMessage('Another error');
-      expect(hintLog.flush()).toEqual(['An error', 'Another error']);
+      var log = hintLog.flush();
+      expect(log['No Name']['An error']).toEqual(['An error']);
+      expect(log['No Name']['Another error']).toEqual(['Another error']);
       expect(hintLog.flush()).toEqual([]);
     });
   });
