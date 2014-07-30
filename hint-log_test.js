62abbd2e716c5567366677073557444bd6c68ae6
Erin Altenhof-Long <ealtenhof@google.com>
2014-07-30 14:39:33 -0700

    test(): update syntax in tests
    Messages logged with logMessage should contain the name of the module delimited as
    example, AngularHintDirectives should prefix logMessage calls with ##Directives##.
    The `Angular Hint` portion of the title is added by AngularHint. The syntax used
    in the hintLog tests now reflects this practice.

--- hint-log_test.js
+++ hint-log_test.js
@@ -3,25 +3,25 @@ var hintLog = require('./hint-log');
 describe('hintLog', function() {
 
   afterEach(function() {
-    expect(hintLog.flush()).toEqual([]);
+    expect(hintLog.flush()).toEqual({});
   });
 
   describe('logMessage', function() {
     it('should add a new message to the message queue', function() {
-      hintLog.logMessage('##Hint Directives## An error');
-      expect(hintLog.flush()['Hint Directives']['An error']).toEqual('An error');
+      hintLog.logMessage('##Directives## An error');
+      expect(hintLog.flush()['Directives']['An error']).toEqual('An error');
     });
 
 
     it('should identify the name of a module given between ##identifiers##', function() {
-      hintLog.logMessage('##Hint Dom## An error');
-      expect(hintLog.flush()['Hint Dom']['An error']).toEqual('An error');
+      hintLog.logMessage('##Dom## An error');
+      expect(hintLog.flush()['Dom']['An error']).toEqual('An error');
     });
 
 
     it('should strip leading white space when using ##identifiers##', function() {
-       hintLog.logMessage('##Hint Dom## An error');
-       expect(hintLog.flush()['Hint Dom']['An error']).toEqual('An error');
+       hintLog.logMessage('##Dom## An error');
+       expect(hintLog.flush()['Dom']['An error']).toEqual('An error');
        hintLog.logMessage('##General## Another error');
        hintLog.logMessage('Message without identifiers');
        expect(Object.keys(hintLog.flush()['General'])).toEqual(['Another error',
@@ -37,14 +37,14 @@ describe('hintLog', function() {
 
     it('should prevent the logging of duplicate messages', function() {
       //Same error, only logged once
-      hintLog.logMessage('##Hint Dom## An error');
-      hintLog.logMessage('##Hint Dom## An error');
-      expect(Object.keys(hintLog.flush()['Hint Dom']).length).toBe(1);
+      hintLog.logMessage('##Dom## An error');
+      hintLog.logMessage('##Dom## An error');
+      expect(Object.keys(hintLog.flush()['Dom']).length).toBe(1);
 
       //Different errors, both logged
-      hintLog.logMessage('##Hint Dom## An error');
-      hintLog.logMessage('##Hint Dom## A second error');
-      expect(Object.keys(hintLog.flush()['Hint Dom']).length).toBe(2);
+      hintLog.logMessage('##Dom## An error');
+      hintLog.logMessage('##Dom## A second error');
+      expect(Object.keys(hintLog.flush()['Dom']).length).toBe(2);
 
       hintLog.logMessage('An error');
       hintLog.logMessage('An error');
